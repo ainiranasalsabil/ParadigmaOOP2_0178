f98d2a543adda420313a9355082eb9a47cc0a945
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2024-05-28 12:05:05 +0700

    class orang

---
+++ solveMultivalue/solveMultivalue.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class Orang
+{
+public:
+    int umur;
+
+    Orang(int pUmur) :
+        umur(pUmur)
+    {
+        cout << "Orang dibuat dengan umur " << umur << "\n" << endl;
+    }
+};
+
