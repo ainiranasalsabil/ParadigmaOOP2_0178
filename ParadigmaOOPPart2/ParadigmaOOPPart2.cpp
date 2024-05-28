735888047be83dc3306bd48826e198fac790507a
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2024-05-28 12:02:38 +0700

    pure virtual function

---
+++ ParadigmaOOPPart2/ParadigmaOOPPart2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class seseorang
+{
+public:
+    virtual void pesan() = 0; //pure virtual function
+
+    // virtual function biasa
+    //virtual void pesan()
+    // {
+    // cout << "pesan dari seseorang" << endl;
+    // }
+};
+
