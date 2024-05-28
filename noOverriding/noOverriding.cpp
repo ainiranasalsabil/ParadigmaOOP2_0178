5af56f11f903247af2622ed86f8d0c98945570f7
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2024-05-28 12:06:59 +0700

    derived class

--- noOverriding/noOverriding.cpp
+++ noOverriding/noOverriding.cpp
@@ -11,3 +11,9 @@ public:
 		cout << "Halo saya Function dari base class";
 	}
 };
+class derivedClass : public baseClass {
+public:
+	void perkenalan() {
+		cout << "Halo saya Function dari derived Class";
+	}
+};
