f337ecce798496f89be8765bcf5a0384d09327f4
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2024-05-28 12:10:38 +0700

    contoh  program abstraksi

---
+++ abstraksi/abstraksi.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class AbstraksiKlass {
+private: string x, y;
+
+public:
+    // method untuk mengisi nilai
+    // private member
+    void setXY(string a, string b) {
+        x = a;
+        y = b;
+    }
+
+    //menampilkan nilai
+    void display() {
+        cout << "x = " << x << endl;
+        cout << "y = " << y << endl;
+    }
+};
+
+int main()
+{
+    AbstraksiKlass ak;
+    ak.setXY("Yogyakarta", "Kampus");
+    ak.display();
+
+    return 0;
+}
